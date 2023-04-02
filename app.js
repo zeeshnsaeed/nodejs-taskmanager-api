069b7439eb796038eed37db7127d61e4b768cd37
Zeeshan Saeed <zsaeed830@gmail.com>
2023-04-02 17:18:13 +0500

    feat: Adding custom error handlers

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/notFound");
+const errorHandlerMiddleware = require("./middleware/errorHandler");
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // Middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // Routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,82 +1,62 @@
 const Task = require("../models/Task");
+const asyncHandler = require("../middleware/async");
+const { createCustomError } = require("../errors/customError");
 
 // Create a new Task   C => CRUD
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-    console.log(task);
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+const createTask = asyncHandler(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
 // Get all Task     R => CRUD
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(201).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-};
+const getAllTasks = asyncHandler(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(201).json({ tasks });
+});
 
 //  Get a single Task
-const getTask = async (req, res) => {
-  // res.json({ id: req.params.id });
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID });
+const getTask = asyncHandler(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
 
-    if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `Task doesn't exist with id : ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  if (!task) {
+    return next(
+      createCustomError(`Task doesn't exist with id : ${taskID}`, 404)
+    );
   }
-};
+  res.status(200).json({ task });
+});
 
 // Update Task Handler  U => CRUD
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+const updateTask = asyncHandler(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `Task doesn't exist with id : ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  if (!task) {
+    return next(
+      createCustomError(`Task doesn't exist with id : ${taskID}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ task });
+});
 
 // Delete Task Handler  D => CRUD
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
+const deleteTask = asyncHandler(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
 
-    if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `Task doesn't exist with id : ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  if (!task) {
+    return next(
+      createCustomError(`Task doesn't exist with id : ${taskID}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ task });
+});
 
 module.exports = {
   getAllTasks,
